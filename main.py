e7aa91c786322701e74c0b65b6ccffaf4da71b02
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 17:20:29 -0300

    Feat: Updating and deleting pixels from pixela

--- main.py
+++ main.py
@@ -40,7 +40,18 @@ pixel_data = {
     "date": today.strftime("%Y%m%d"),
     "quantity": "15.0"
 }
-print(today.strftime("%Y%m%d"))
+# print(today.strftime("%Y%m%d"))
+#
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
 
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.delete(url=delete_endpoint, headers=headers)
 print(response.text)
